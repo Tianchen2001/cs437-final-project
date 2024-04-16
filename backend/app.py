ba21acb23254f61d45e0ac9f2acab35c8bf9f63b
Tianchen Wang <tw17@illinois.edu>
2024-04-16 11:12:24 -0500

    update backend parking system

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,25 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from parking_system import ParkingSystem
+
 app = Flask(__name__)
 CORS(app)
+
+ps = ParkingSystem()
 
 @app.route('/move', methods=['POST'])
 def move():
     data = request.get_json()
     slot_number = data.get('slot')
     print(f"Moving car to slot {slot_number}")
+    ps.move_to(slot_number)
     return jsonify({'message': f'Car moved to slot {slot_number}'}), 200
 
 @app.route('/exit', methods=['POST'])
 def exit():
     print("Car is exiting the parking lot")
+    ps.move_to(None)
     return jsonify({'message': 'Car exited the parking lot'}), 200
 
 if __name__ == '__main__':
---
+++ backend/parking_system.py
@@ -0,0 +1,35 @@
+# import picar_4wd as fc
+import numpy as np
+
+
+class ParkingSystem:
+    def __init__(self, num_parking_slots=10, center_width=5):
+        self.num_rows, self.num_cols = num_parking_slots // 2, center_width + 2
+        
+        self.parking_lot_map = np.zeros((self.num_rows, self.num_cols))
+        self.parking_lot_map[1:self.num_rows-1, 1:self.num_cols-1] = 1
+        
+        self.current_slot = None
+    
+    def move_to(self, target_slot):
+        if self.current_slot == target_slot:
+            return
+        
+        target_pos = None
+        if target_slot is None:
+            target_pos = (self.num_rows - 1, self.num_cols // 2)
+        else:
+            target_pos = self._get_pos_by_slot(target_slot)
+        print(target_pos, self.parking_lot_map[target_pos])
+        
+        '''
+            TODO: Implement path planning algorithm.
+            The car moves counter-clockwise around the parking lot.
+            Whenever the car reaches the target slot, it stops and turns right to do a head-in parking.
+            The scenario also applies when the car is exiting the parking lot.
+        '''
+        
+        self.current_slot = target_slot
+        
+    def _get_pos_by_slot(self, slot):
+        return (slot - 1) % 5, self.num_cols - 1 if slot > 5 else 0
